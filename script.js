b4c39cae12ca4dc265da91361a4ad4fdd3e906c1
warisha234 <asmaturab3@gmail.com>
2025-09-11 19:19:35 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,138 @@
+// hamburger k liye
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
+// typing effect k liye
+const roles = ["Web Developer", "Video Editor", "Calligrapher"];
+let roleIndex = 0;
+
+function showRole() {
+  const roleElement = document.getElementById("role");
+  roleElement.textContent = roles[roleIndex];
+  roleElement.classList.remove("typing");
+
+  void roleElement.offsetWidth;
+  roleElement.classList.add("typing");
+
+  roleIndex = (roleIndex + 1) % roles.length;
+}
+showRole();
+setInterval(showRole, 2000);
+
+// paragraph ko read more k liye
+function readMore() {
+  let moreText = document.getElementById("moreText");
+  let btn = document.getElementById("readmore");
+
+  if (moreText.style.display === "none") {
+    moreText.style.display = "inline";
+    btn.innerHTML = "Read Less";
+  } else {
+    moreText.style.display = "none";
+    btn.innerHTML = "Read More";
+  }
+}
+
+
+
+// projects scroll  k liye
+const cards = document.querySelectorAll(".project-card");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.2 });
+
+
+cards.forEach(card => observer.observe(card));
+
+
+// certificate scroll k liye
+const certCards = document.querySelectorAll('.certificate-card');
+
+function showCertificates() {
+  certCards.forEach((card) => {
+    const cardPos = card.getBoundingClientRect().top;
+    const screenPos = window.innerHeight / 1.2;
+    if (cardPos < screenPos) {
+      card.classList.add('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', showCertificates);
+
+
+// experience scroll k liye
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const containers = document.querySelectorAll(".container");
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target); 
+        }
+      });
+    }, { threshold: 0.2 });
+
+    containers.forEach((container) => {
+      observer.observe(container);
+    });
+  });
+
+// my skills k liye
+document.addEventListener("DOMContentLoaded", () => {
+  const skillBoxes = document.querySelectorAll(".skill");
+  const progressBars = document.querySelectorAll(".progress");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+
+        let progress = entry.target.querySelector(".progress");
+        if (progress) {
+          progress.style.width = progress.getAttribute("data-width");
+        }
+      }
+    });
+  }, { threshold: 0.3 });
+
+  skillBoxes.forEach(box => observer.observe(box));
+});
+
+
+// testimonials slider k liye
+  let index = 0;
+  const testimonials = document.querySelectorAll(".testimonial-card");
+  const total = testimonials.length;
+
+  function showTestimonial(i) {
+    testimonials.forEach((t, idx) => {
+      t.classList.remove("active");
+      if (idx === i) t.classList.add("active");
+    });
+  }
+
+  document.querySelector(".next").addEventListener("click", () => {
+    index = (index + 1) % total;
+    showTestimonial(index);
+  });
+
+  document.querySelector(".prev").addEventListener("click", () => {
+    index = (index - 1 + total) % total;
+    showTestimonial(index);
+  });
+
+
+  setInterval(() => {
+    index = (index + 1) % total;
+    showTestimonial(index);
+  }, 5000);
+
