aa30462e108c8edbee861de26030368dcccf66e7
warisha234 <asmaturab3@gmail.com>
2026-02-09 09:30:06 +0500

    some changes on portfolio

--- script.js
+++ script.js
@@ -12,7 +12,24 @@ window.addEventListener('DOMContentLoaded', () => {
   }, 3000);
 });
 
+  const backToTopButton = document.getElementById("backToTop");
 
+  // Show button after scrolling 200px
+  window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  };
+
+  // Scroll to top on click
+  backToTopButton.addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" // smooth scroll
+    });
+  });
 
 
 // hamburger k liye
@@ -146,3 +163,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+ const form = document.getElementById("contact-form");
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault(); // page reload ko rokte hain
+
+    // SweetAlert popup
+    Swal.fire({
+      icon: 'success',
+      title: 'Message Sent!',
+      text: 'Your message has been submitted successfully.',
+      showConfirmButton: true,
+      confirmButtonText: 'OK',
+      background: '#03102e',
+      color: '#fff',
+    });
+
+    form.reset(); // form clear ho jaye
+  });
+
+
+  
